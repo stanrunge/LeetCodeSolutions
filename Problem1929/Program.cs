eb0226fabb6fbdb2e3fec665d28aa763a4ff8091
stanrunge <stan@stanrunge.dev>
2022-12-28 21:43:05 +0100

    Solve problem 1929

---
+++ Problem1929/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem1929;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+    
+    public static int[] GetConcatenation(int[] nums)
+    {
+        var ans = new int[nums.Length * 2];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[i];
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            ans[i + nums.Length] = nums[i];
+        }
+
+        return ans;
+    }
+}
