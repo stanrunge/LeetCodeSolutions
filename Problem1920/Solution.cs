0087f9c437c627200c07b50c3604a6d235bb6eb3
Stan <stanrunge03@gmail.com>
2022-12-29 13:25:52 +0100

    Add solution to problem 1920

---
+++ Problem1920/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Problem1920;
+
+public abstract class Solution {
+    public static void Main(string[] args)
+    {
+        var array = new int[6];
+        BuildArray(array);
+    }
+
+    private static int[] BuildArray(int[] nums) {
+        var ans = new int[nums.Length];
+
+        for(var i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
