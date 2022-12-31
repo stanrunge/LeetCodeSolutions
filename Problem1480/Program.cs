de0e14a5e485d1023c5e99c08afc19e5a424e583
Stan <stanrunge03@gmail.com>
2022-12-31 21:00:20 +0100

    Solve problem 1480

---
+++ Problem1480/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem1480;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+
+    private static int[] RunningSum(int[] nums)
+    {
+        var answer = new int[nums.Length];
+        answer[0] = nums[0];
+        
+        for (var i = 1; i < nums.Length; i++)
+        {
+            answer[i] = answer[i - 1] + nums[i];
+        }
+
+        return answer;
+    }
+}
