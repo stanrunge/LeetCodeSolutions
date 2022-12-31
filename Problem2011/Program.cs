36f0cf4ca3a431393a4b19dec162ac453e5c7031
Stan <stanrunge03@gmail.com>
2022-12-31 21:07:50 +0100

    Solve problem 2011

---
+++ Problem2011/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem2011;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+
+    private static int FinalValueAfterOperaions(string[] operations)
+    {
+        var increments = operations.ToList().FindAll(x => x == "++X" || x == "X++").Count();
+        var decrements = operations.ToList().FindAll(x => x == "--X" || x == "X--").Count();
+
+        return increments - decrements;
+    }
+}
