0136d8ab7857211d6a013bfa3691963c430078cb
Stan <stanrunge03@gmail.com>
2022-12-31 20:59:56 +0100

    Solve problem 1108

---
+++ Problem1108/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem1108;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+
+    private static string DefangIPaddr(string address)
+    {
+        return address.Replace(".", "[.]");
+    }
+}
