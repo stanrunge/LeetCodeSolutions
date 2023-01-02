b5b212578386bc7a55975dc6b41f6b8379398f4f
stanrunge <stan@stanrunge.dev>
2023-01-02 18:49:49 +0100

    Solve problem 1470

---
+++ Problem1470/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem1470;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3);
+    }
+
+    private static int[] Shuffle(int[] nums, int n)
+    {
+        var answer = new int[nums.Length];
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                answer[i] = nums[i / 2];
+            }
+            else
+            {
+                answer[i] = nums[((i - 1) / 2) + nums.Length / 2];
+            }
+        }
+
+        return answer;
+    }
+}
