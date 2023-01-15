7984355b5ae0f63d91a7db05fbce60eeb8754f82
Stan Runge <stan@stanrunge.dev>
2023-01-15 21:52:38 +0100

    Solve problem 1528

---
+++ Problem1528/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem1528;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().RestoreString("codeleet", new []{4,5,6,7,0,2,1,3}));
+    }
+}
+
+public class Solution {
+    public string RestoreString(string s, int[] indices)
+    {
+        var shuffledString = "";
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var index = Array.FindIndex(indices, integer => integer == i);
+
+            shuffledString += s[index];
+        }
+
+        return shuffledString;
+    }
+}
